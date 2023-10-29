57749821950577f8ceaac55814897cf162f5d6b6
Ricardo Marçal <ricardomarcal02@hotmail.com>
2023-10-29 13:24:56 -0300

    fixes

--- src/JRMIDecoder.java
+++ src/JRMIDecoder.java
@@ -23,54 +23,22 @@ public class JRMIDecoder {
 
     public void execute(String mensagem, String[] params) {
         switch (mensagem.toLowerCase()) {
-            case "titulo":
-                titulo(params);
-                break;
-            case "autor":
-                autor(params);
-                break;
-            case "desc":
-                descricao(params);
-                break;
-            case "c":
-                tocarNota(Notas.C, params);
-                break;
-            case "c#":
-                tocarNota(Notas.CS, params);
-                break;
-            case "d":
-                tocarNota(Notas.D, params);
-                break;
-            case "d#":
-                tocarNota(Notas.DS, params);
-                break;
-            case "e":
-                tocarNota(Notas.E, params);
-                break;
-            case "f":
-                tocarNota(Notas.F, params);
-                break;
-            case "f#":
-                tocarNota(Notas.FS, params);
-                break;
-            case "g":
-                tocarNota(Notas.G, params);
-                break;
-            case "g#":
-                tocarNota(Notas.GS, params);
-                break;
-            case "a":
-                tocarNota(Notas.A, params);
-                break;
-            case "a#":
-                tocarNota(Notas.AS, params);
-                break;
-            case "b":
-                tocarNota(Notas.B, params);
-                break;
-            case "x":
-                pausa(params);
-                break;
+            case "titulo" -> titulo(params);
+            case "autor" -> autor(params);
+            case "desc" -> descricao(params);
+            case "c" -> tocarNota(Notas.C, params);
+            case "c#" -> tocarNota(Notas.CS, params);
+            case "d" -> tocarNota(Notas.D, params);
+            case "d#" -> tocarNota(Notas.DS, params);
+            case "e" -> tocarNota(Notas.E, params);
+            case "f" -> tocarNota(Notas.F, params);
+            case "f#" -> tocarNota(Notas.FS, params);
+            case "g" -> tocarNota(Notas.G, params);
+            case "g#" -> tocarNota(Notas.GS, params);
+            case "a" -> tocarNota(Notas.A, params);
+            case "a#" -> tocarNota(Notas.AS, params);
+            case "b" -> tocarNota(Notas.B, params);
+            case "x" -> pausa(params);
         }
     }
 
